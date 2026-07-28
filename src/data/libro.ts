5d652c53efab31306bebefbdbfd8af581befc9a2
Francisco M. López Martínez <franciscomlopez.escritor@gmail.com>
2026-07-28 23:15:53 +0200

    Actualiza todos los enlaces de compra de Amazon a la nueva URL de afiliado y los sincroniza en Schema.org

--- src/data/libro.ts
+++ src/data/libro.ts
@@ -5,7 +5,7 @@ export const libro = {
   edicion: "2ª Edición",
   editorial: "Letras del Alma Ediciones",
   genero: "Novela negra / Thriller psicológico",
-  enlaceCompra: "https://www.amazon.es/Paradoja-%C2%ABthriller%C2%BB-psicol%C3%B3gico-alto-voltaje-ebook/dp/B0G5ZLNPX7?ref_=ast_author_mpb",
+  enlaceCompra: "https://www.amazon.es/Paradoja-%C2%ABthriller%C2%BB-psicol%C3%B3gico-alto-voltaje-ebook/dp/B0G5ZLNPX7?dib=eyJ2IjoiMSJ9._sOpIG5Z9hs2YJq9u4t_w-NebJch14tYNYxs2I8-oGiKkT2DT-opm_g4JiPkli9_bHrgB5BkELsEyS1fCpX5D5-uEqSbQwkajiqN3rk-SBIxiJ2bMWOfFqpMoGhWs4US3-sMd9wfAaAgIIjCjsiVykpASTpfnKAza7L7XgC37cx-dxo4Omf1avWqtr7xZ9-93EFkxDcGfoBrIc5s8TVRDup330z8qAECE397tZfNaKOxZmumqtaBPi2Q00_alwC7RJktB_Z0xz2dJSPNrkHNr7lFuNioPm_Hrj2n13515xw.1roA-Bm50b7rJs7JOVaeF8_9XkmmGXWDqeAH03UUSRY&dib_tag=se&keywords=la+paradoja&qid=1785273278&sr=8-2&linkCode=ll2&tag=tebeitos-21&linkId=a194995031a919251bba99324fea699a&ref_=as_li_ss_tl",
 };
 
 export const autor = {
